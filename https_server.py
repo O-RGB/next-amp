977c80a17dcb2ce252a8c4fb2942f1f600531904
phichayoot <phichayoot@digitiveworks.com>
2025-12-23 19:47:27 +0700

    แก้ไขโปรแกรมมีปัญหา

---
+++ https_server.py
@@ -0,0 +1,14 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import ssl
+
+PORT = 8444
+
+httpd = HTTPServer(("0.0.0.0", PORT), SimpleHTTPRequestHandler)
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+print(f"HTTPS running at https://localhost:{PORT}")
+httpd.serve_forever()
