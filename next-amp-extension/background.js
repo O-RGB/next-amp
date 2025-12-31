2585cbfc4059c3c9d7c4a583b76419cc9ac31a22
phichayoot <phichayoot@digitiveworks.com>
2025-12-31 20:27:17 +0700

    update

--- next-amp-extension/background.js
+++ next-amp-extension/background.js
@@ -29,16 +29,48 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 });
 
 chrome.runtime.onMessageExternal.addListener((msg, sender, sendResponse) => {
-  if (
-    msg.type === "SET_PARAM" ||
-    msg.type === "START_CAPTURE" ||
-    msg.type === "STOP_CAPTURE"
-  ) {
+  if (msg.type === "PING") {
+    sendResponse({
+      status: "PONG",
+      version: chrome.runtime.getManifest().version,
+    });
+    return false;
+  }
+
+  if (msg.type === "START_CAPTURE") {
+    if (!sender || !sender.tab) {
+      sendResponse({ success: false, error: "No sender tab" });
+      return false;
+    }
+
+    chrome.tabCapture.getMediaStreamId(
+      { targetTabId: sender.tab.id },
+      (streamId) => {
+        if (chrome.runtime.lastError || !streamId) {
+          console.error("Error getting stream ID:", chrome.runtime.lastError);
+          sendResponse({
+            success: false,
+            error: chrome.runtime.lastError?.message,
+          });
+          return;
+        }
+
+        chrome.runtime.sendMessage({
+          type: "START_CAPTURE",
+          streamId: streamId,
+          tabId: sender.tab.id,
+        });
+
+        sendResponse({ success: true });
+      }
+    );
+    return true;
+  } else if (msg.type === "STOP_CAPTURE" || msg.type === "SET_PARAM") {
     chrome.runtime.sendMessage(msg);
     sendResponse({ success: true });
   } else if (msg.type === "GET_STATE") {
     chrome.runtime.sendMessage({ type: "GET_STATE" }, (response) => {
-      sendResponse(response);
+      sendResponse(response || {});
     });
     return true;
   }
